7701fadee6fb5dc95de0244d95499238c89e17d9
Farrel <farrelacc2@gmail.com>
2025-10-09 12:23:20 +0700

    Debugging

--- sporra/urls.py
+++ sporra/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    
+    path('', RedirectView.as_view(url='/news/', permanent=True)),  #SHOW NEWS AS MAIN URL FOR DEVELOPMENT PURPOSES
+    
+    
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),
 ]
