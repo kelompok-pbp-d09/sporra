d513f5792742d94cfe0cc1fc29c7df9ca9952dc4
Farrel <farrelacc2@gmail.com>
2025-10-22 20:16:14 +0700

    Fixed login and register

--- profile_user/views.py
+++ profile_user/views.py
@@ -1,35 +1,48 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-# Create your views here.
 
-def register(request):
-    form = UserCreationForm()
-
-    if request.method == "POST":
+def register_user(request):
+    if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Your account has been successfully created!')
-            return redirect('profile_user:login')
-    context = {'form':form}
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Akun {user.username} berhasil dibuat!')
+            return redirect('news:article-list')
+    else:
+        form = UserCreationForm()
+        
+    context = {'form': form}
     return render(request, 'register.html', context)
 
+
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Selamat datang kembali, {username}!")
+                return redirect('news:article-list')
+            else:
+                messages.error(request, "Username atau password salah.")
+        else:
+            messages.error(request, "Username atau password salah.")
+    else:
+        form = AuthenticationForm()
+        
+    return render(request, 'login.html', {'form': form})
 
-      if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('profile_user:register')
-
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
-    return redirect('profile_user:login')   
+    messages.info(request, "Kamu telah berhasil logout.")
+    return redirect('landing-page')
