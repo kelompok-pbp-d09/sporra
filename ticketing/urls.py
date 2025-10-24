539a6ecd503da1d78cf7be37db1bb392540f7355
Zenixalarate01 <dylan.pirade@ui.ac.id>
2025-10-24 12:19:13 +0700

    migrate agar tidak error

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -6,7 +6,11 @@ app_name = 'ticketing'
 urlpatterns = [
     path('book/<uuid:event_id>/', views.book_ticket, name='book_ticket'),  
     path('my-bookings/', views.my_bookings, name='my_bookings'),
-    path('tickets/data/', views.get_tickets, name='get_tickets'),    # endpoint ajax
+    path('tickets/data/', views.get_tickets_ajax, name='get_tickets'),    # endpoint ajax
     path('tickets/', views.all_tickets, name='all_tickets'),
     path('create/', views.create_ticket, name='create_ticket'),
+    path('ajax/my_bookings/', views.get_my_bookings_ajax, name='get_my_bookings_ajax'),  # JSON untuk AJAX
+    path('edit_ticket_ajax/<int:ticket_id>/', views.edit_ticket_ajax, name='edit_ticket_ajax'),  # Edit via AJAX
+    path('delete_ticket_ajax/<int:ticket_id>/', views.delete_ticket_ajax, name='delete_ticket_ajax'),  # Delete via AJAX
+
 ]
