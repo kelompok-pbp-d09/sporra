fe21ddf75663f6dd2a46dd0ae35b88523607c0cf
Dylan Pirade Ponglabba <dylan.pirade@ui.ac.id>
2025-11-30 10:27:17 +0700

    Original urls

--- profile_user/urls.py
+++ profile_user/urls.py
@@ -4,9 +4,6 @@ from profile_user.views import *
 app_name = 'profile_user'
 
 urlpatterns = [
-    path('auth/login/', login_flutter, name='login_flutter'),
-    path('auth/register/', register_flutter, name='register_flutter'),
-    path('auth/logout/', logout_flutter, name='logout_flutter'),
     path('', show_profile, name='show_profile'),
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
@@ -15,7 +12,10 @@ urlpatterns = [
     path('add_status/', add_status, name='add_status'),
     path('delete_status/<int:status_id>/', delete_status, name='delete_status'),
     path('edit_status/<int:status_id>/', edit_status, name='edit_status'),
+    path('auth/login/', login_flutter, name='login_flutter'),
+    path('auth/register/', register_flutter, name='register_flutter'),
+    path('auth/logout/', logout_flutter, name='logout_flutter'),
+    path('<str:username>/', show_profile, name='show_profile'),
+    path('<int:id>/', show_profile, name='show_profile'),
     path('user/<str:username>/', show_profile, name='user_profile'),
-    path('<str:username>/', show_profile, name='show_profile_by_username'),
-    path('<int:id>/', show_profile, name='show_profile_by_id'),
 ]
